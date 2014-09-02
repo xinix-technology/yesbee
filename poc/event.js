7c4e149bff4af33683368617ea1828a4e391e039
Ganesha <reekoheek@gmail.com>
2014-09-02 09:19:50 +0700

    introduced event

--- poc/event.js
+++ poc/event.js
@@ -2,8 +2,8 @@ var Context = require('./lib/context');
 
 var context = new Context();
 
-// var route = context.from('mock:input')
-    // .to('mock:output');
+// var route = context.from('direct:input')
+    // .to('direct:output');
 
 context.on('xxx', function() {
     console.log('xxx');
