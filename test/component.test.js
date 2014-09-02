fda8cbbea0d3707acdd9cc75b29be53666c43e01
Ganesha <reekoheek@gmail.com>
2014-09-02 09:18:49 +0700

    fixed component

--- test/component.test.js
+++ test/component.test.js
@@ -1,42 +1,288 @@
-var assert = require("assert");
+var chai = require('chai'),
+    expect = require('chai').expect,
+    Component = require('../lib/component'),
+    Context = require('../lib/context'),
+    Exchange = require('../lib/exchange'),
+    spies = require('chai-spies');
 
-var Component = require('../lib/component');
+chai.use(spies);
 
 describe('Component', function(){
-    describe('.register()', function() {
+    var context;
+
+    beforeEach(function(){
+        context = new Context();
+    });
+
+    describe('#register()', function() {
         it('should able to register custom protocol', function() {
             Component.register('custom-protocol', {});
-            assert.equal(typeof Component.registries['custom-protocol:'], 'object');
+            expect(Component.registries['custom-protocol:']).to.be.an('object');
         });
     });
 
-    describe('.create()', function(){
-        it('should throw error on unknown uri', function() {
-            var c;
-            try {
-                c = Component.create(null);
-                throw new Error('Unthrown error');
-            } catch(e) {
-            }
-
-            try {
-                c = Component.create(null, 'foo');
-                throw new Error('Unthrown error');
-            } catch(e) {
-            }
+    describe('#processor()', function() {
+        it('should able to create processor-type component', function() {
+            var component = Component.processor(function() { });
+            expect(component).to.be.instanceof(Component);
         });
 
-        it('should throw error on unknown unregistered protocol', function() {
-            try {
-                var c = Component.create(null, 'bar:xxx');
-                throw new Error('Unthrown error');
-            } catch(e) {
-            }
+        it('should throw error on non function argument', function() {
+            expect(function() {
+                var component = Component.processor();
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = Component.processor('direct:x');
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = Component.processor({});
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = Component.processor([]);
+            }).to.throw(Error);
+        });
+    });
+
+    describe('new', function() {
+        it('should create new instance of component', function() {
+            var component = new Component('direct:test');
+            expect(component).to.be.instanceof(Component);
         });
 
-        it('should return mock component with mock uri', function() {
-            var c = Component.create(null, 'mock:test');
-            assert.equal(typeof c, 'object');
+        it('should throw error on non string uri argument', function() {
+            expect(function() {
+                var component = new Component(function() {});
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = new Component({});
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = new Component(123);
+            }).to.throw(Error);
         });
+    });
+
+    describe('.start()', function() {
+        it('should throw error if detached from context', function() {
+            expect(function() {
+                var component = new Component('direct:test');
+                component.start();
+            }).to.throw(Error);
+        });
+
+        it('should set status to 1', function() {
+            var component = context.createComponent('direct:test');
+
+            expect(component.status).to.be.equal(0);
+
+            component.start();
+
+            expect(component.status).to.be.equal(1);
+        });
+    });
+
+    describe('.stop()', function() {
+        it('should set status to 0', function() {
+            var component = context.createComponent('direct:test');
+
+            component.start();
+            expect(component.status).to.be.equal(1);
+
+            component.stop();
+            expect(component.status).to.be.equal(0);
+
+        });
+    });
+
+    describe('.process()', function() {
+        it('has default process() method', function() {
+            var component = new Component('direct:test');
+
+            expect(component.process).to.be.a('function');
+        });
+    });
+
+    describe('.send()', function() {
+        it('has default send() method', function() {
+            var component = new Component('direct:test');
+
+            expect(component.send).to.be.a('function');
+        });
+
+        it('should throw error if detached from context', function() {
+            expect(function() {
+                var component = new Component('direct:test');
+                component.send();
+            }).to.throw(Error);
+        });
+
+        // TODO some new specs to be implemented later
+        // it should hold the message for some moment until receiver alive
+        // it should error if no receiver alive for several time
+    });
+
+    describe('.callback()', function() {
+        it('has default callback() method', function() {
+            var component = new Component('direct:test');
+
+            expect(component.callback).to.be.a('function');
+        });
+
+        // TODO some new specs
+        // it should reach callback on last component send in inout route
+    });
+
+    describe('.initialize()', function() {
+        it('has default initialize() method', function() {
+            var component = new Component('direct:test');
+
+            expect(component.initialize).to.be.a('function');
+        });
+    });
+
+    describe('.getNext()', function() {
+        it('should return next processor', function() {
+            var component = context.createComponent('direct:source'),
+                next = context.createComponent('direct:destination');
+            component.next = next;
+
+            expect(component.getNext()).to.be.equal(next);
+        });
+
+        it('should return null if no next processor', function() {
+            var component = context.createComponent('direct:source');
+            expect(component.getNext()).to.be.null;
+        });
+    });
+
+    describe('.doInit()', function() {
+        // noop
+    });
+
+    describe('.doProcess()', function() {
+        it('should return promise', function() {
+            var component = context.createComponent('direct:1');
+            var result = component.doProcess(new Exchange());
+            expect(result).not.to.be.null;
+            expect(result.then).to.be.a('function');
+        });
+
+        it('should call .process()', function() {
+            var spy = chai.spy(),
+                component = Component.processor(spy),
+                x = new Exchange();
+
+            component.doProcess(x);
+
+            expect(spy).to.have.been.called.with(x);
+        });
+    });
+
+    describe('.doCallback()', function() {
+        it('should call .callback()', function() {
+            var component = context.createComponent('direct:x'),
+                x = new Exchange(),
+                spy = chai.spy(component.callback);
+
+            component.callback = spy;
+
+            component.doCallback(x);
+
+            expect(spy).to.have.been.called.once.with(x);
+        });
+    });
+
+    describe('.doSend()', function() {
+        it('should send to next component', function(done) {
+
+            var spy = chai.spy();
+
+            var route = context.from('direct:a')
+                .to(spy);
+
+            // context.trace = true;
+            context.start();
+
+            var template = context.createProducerTemplate();
+            var ex = template.createExchange('test-' + new Date());
+            ex.pattern = 'inOnly';
+
+            template.send('direct:a', ex);
+
+            setImmediate(function() {
+                expect(spy).to.have.been.called.once();
+
+                done();
+            });
+
+        });
+
+        it('should send to next component and purge to SINK for inOnly', function(done) {
+
+            var spy = chai.spy(),
+                spy2 = chai.spy();
+
+            var route = context.from('direct:a')
+                .to(spy);
+
+            context.on('::SINK', spy2);
+
+            // context.trace = true;
+            context.start();
+
+            var template = context.createProducerTemplate();
+            var ex = template.createExchange('test-' + new Date());
+            ex.pattern = 'inOnly';
+
+            template.send('direct:a', ex);
+
+            setImmediate(function() {
+                expect(spy).to.have.been.called.once();
+                expect(spy2).to.have.been.called.once();
+
+                done();
+            });
+
+        });
+
+        it('should send to next component and callback to source for inOut', function(done) {
+
+            var spy = chai.spy(),
+                spy2 = chai.spy(),
+                spy3;
+
+            var route = context.from('direct:a')
+                .to(spy);
+
+            context.on('::SINK', spy2);
+
+            var c = route.query('direct:a');
+            spy3 = chai.spy(c.callback);
+            c.callback = spy3;
+
+            // context.trace = true;
+            context.start();
+
+            var template = context.createProducerTemplate();
+            var ex = template.createExchange('test-' + new Date());
+            ex.pattern = 'inOut';
+
+            template.send('direct:a', ex);
+
+            setImmediate(function() {
+                expect(spy).to.have.been.called.once();
+                expect(spy2).to.have.not.been.called.once();
+                expect(spy3).to.have.been.called.once();
+
+                done();
+            });
+
+        });
+
     });
 });
