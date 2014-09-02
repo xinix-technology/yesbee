e3823ef5fee13363385000f93fb86ace18e36c17
Ganesha <reekoheek@gmail.com>
2014-09-02 09:20:26 +0700

    wrote unittests

--- test/context.test.js
+++ test/context.test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect,
     Route = require('../lib/route'),
     Context = require('../lib/context'),
+    Component = require('../lib/component'),
     ProducerTemplate = require('../lib/producer-template'),
     _ = require('lodash');
 
@@ -19,7 +20,7 @@ describe('Context', function(){
 
     describe('.from()', function(){
         it('should create a route', function() {
-            var route = context.from('mock:test');
+            var route = context.from('direct:test');
 
             expect(route).to.be.an('object');
             expect(route).to.be.an.instanceof(Route);
@@ -27,30 +28,34 @@ describe('Context', function(){
     });
 
     describe('.start()', function() {
-        it('should start all input components', function() {
-            context.from('mock:test')
-                .to('mock:result');
+        it('should start all input and output components', function() {
+            context.from('direct:test')
+                .to('direct:result');
 
             context.start();
 
             _.each(context.routes, function(route) {
-                _.each(route.inputs, function(input) {
-                    expect(input.status).to.equal(1);
+                expect(route.source.status).to.equal(1, 'Inputs should started');
+
+                _.each(route.processors, function(output) {
+                    expect(output.status).to.equal(1, 'Outputs should started');
                 });
             });
         });
     });
 
     describe('.stop()', function() {
-        it('should stop all input components', function() {
-            context.from('mock:test')
-                .to('mock:result');
+        it('should stop all input and output components', function() {
+            context.from('direct:test')
+                .to('direct:result');
 
             context.stop();
 
             _.each(context.routes, function(route) {
-                _.each(route.inputs, function(input) {
-                    expect(input.status).to.equal(0);
+                expect(route.source.status).to.equal(0, 'Inputs should stopped');
+
+                _.each(route.processors, function(output) {
+                    expect(output.status).to.equal(0, 'Outputs should stopped');
                 });
             });
         });
@@ -79,6 +84,58 @@ describe('Context', function(){
             var template = context.createProducerTemplate();
 
             expect(template.status).to.be.equal(1);
+        });
+    });
+
+    describe('.createComponent()', function() {
+        it('should return component', function() {
+            var component = context.createComponent('direct:a');
+            expect(component).to.be.instanceof(Component);
+        });
+
+        it('should return processor component if the first argument is function', function() {
+            var component = context.createComponent(function() {
+
+            });
+            expect(component).to.be.instanceof(Component);
+        });
+
+        it('should throw error on empty argument', function() {
+            expect(function() {
+                var component = context.createComponent();
+            }).to.throw(Error);
+        });
+
+
+        it('should throw error on wrong or unregistered protocol', function() {
+            expect(function() {
+                var component = context.createComponent('foo');
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = context.createComponent('foo:xxx');
+            }).to.throw(Error);
+        });
+    });
+
+    describe('.createSourceComponent()', function() {
+        it('should return component with type source', function() {
+            var component = context.createSourceComponent('direct:a');
+            expect(component).to.be.instanceof(Component);
+            expect(component.type).to.be.equal('source');
+        });
+
+        it('should throw error on already registered component uri as source', function() {
+            expect(function() {
+                var component = context.createSourceComponent('direct:a');
+                component = context.createSourceComponent('direct:a');
+            }).to.throw(Error);
+
+            expect(function() {
+                var component = context.createSourceComponent('direct:b');
+                component = context.createSourceComponent('direct:b?with=options');
+            }).to.throw(Error);
+
         });
     });
 });
--- test/route.test.js
+++ test/route.test.js
@@ -1,49 +1,94 @@
 var expect = require('chai').expect,
+    Route = require('../lib/route'),
+    Context = require('../lib/context'),
     Component = require('../lib/component');
 
 var Route = require('../lib/route');
 
 describe('Route', function(){
+    var context;
+    beforeEach(function() {
+        context = new Context();
+    });
+
     describe('new', function(){
         it('should create route', function() {
             var route = new Route();
-            expect(route).to.be.an('object');
+
+            expect(route).to.be.an.instanceof(Route);
         });
     });
 
-    describe('#from()', function(){
+    describe('.from()', function(){
         it('should accept uri string', function() {
-            var route = new Route();
-            route.from('mock:test');
+            var route = new Route(context);
+            route.from('direct:in?x=a');
 
-            expect(route).to.be.an('object');
-            expect(route.inputs[0].type).to.equal('mock$component');
+            expect(route.source).to.be.an.instanceof(Component);
+
         });
 
-        it('should accept component object', function() {
-            var route = new Route();
-            route.from(Component.create(null, 'mock:test'));
-
-            expect(route).to.be.an('object');
-            expect(route.inputs[0].type).to.equal('mock$component');
+        it('should throw error if context is not specified', function() {
+            expect(function() {
+                var route = new Route();
+                route.from('direct:test');
+            }).to.throw(Error);
         });
     });
 
-    describe('#to()', function(){
+    describe('.to()', function(){
         it('should accept uri string', function() {
-            var route = new Route();
-            route.to('mock:result');
+            var route = new Route(context);
+            route.to('direct:result');
 
-            expect(route).to.be.an('object');
-            expect(route.outputs[0].type).to.equal('mock$component');
+            expect(route.processors[0]).to.be.an.instanceof(Component);
         });
 
-        it('should accept component object', function() {
-            var route = new Route();
-            route.to(Component.create(null, 'mock:test'));
+        it('should throw error if context is not specified', function() {
+            expect(function() {
+                var route = new Route();
+                route.to('direct:test');
+            }).to.throw(Error);
+        });
+    });
 
-            expect(route).to.be.an('object');
-            expect(route.outputs[0].type).to.equal('mock$component');
+    describe('.start()', function(){
+        it('should start inputs', function() {
+            var route = new Route(context);
+            route.from('direct:from')
+                .to('direct:to');
+
+            expect(route.source.status).to.be.equal(0);
+
+            route.start();
+
+            expect(route.source.status).to.be.equal(1);
+        });
+
+        it('should throw error if from not specified', function() {
+            expect(function() {
+                var route = new Route();
+                route.from('direct:to');
+
+                route.start();
+            }).to.throw(Error);
+
+        });
+    });
+
+    describe('.stop()', function(){
+        it('should stop inputs', function() {
+            var route = new Route(context);
+            route.from('direct:from')
+                .to('direct:to');
+
+            route.start();
+
+            expect(route.source.status).to.be.equal(1);
+
+            route.stop();
+
+            expect(route.source.status).to.be.equal(0);
         });
     });
 });
