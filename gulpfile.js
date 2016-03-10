1d70fbf21312d44459510d5e6947c646680b11e2
Ganesha <reekoheek@gmail.com>
2016-03-10 23:25:15 +0700

    REPLACED pas to gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const istanbul = require('gulp-istanbul');
+const mocha = require('gulp-mocha');
+
+gulp.task('test', () => {
+  return gulp.src(['test/**/*.js'])
+    .pipe(mocha())
+    .once('error', () => {
+      process.exit(1);
+    })
+    .once('end', () => {
+      process.exit();
+    });
+});
+
+gulp.task('pre-coverage', () => {
+  return gulp.src(['lib/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage', ['pre-coverage'], () => {
+  return gulp.src(['test/**/*.js'])
+    .pipe(mocha())
+    .pipe(istanbul.writeReports({
+      reporters: [ 'lcov', 'json', 'text', 'text-summary'],
+    }))
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+    .once('error', () => {
+      process.exit(1);
+    })
+    .once('end', () => {
+      process.exit();
+    });
+});
+
+gulp.task('default', () => {
+  console.log('Usage:');
+  console.log('  test....... Run unit test');
+  console.log('  coverage... Run coverage test');
+});
